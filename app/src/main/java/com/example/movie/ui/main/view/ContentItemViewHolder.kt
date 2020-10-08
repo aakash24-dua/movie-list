c388ecdc35c931e678c69d13f594c142a3588d8b
Aakash Dua <aakash24.dua@gmail.com>
2020-10-09 02:09:33 +0530

    First Commit

---
+++ app/src/main/java/com/example/movie/ui/main/view/ContentItemViewHolder.kt
@@ -0,0 +1,24 @@
+package com.example.movie.ui.main.view
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movie.R
+import com.example.movie.ui.main.model.Movie
+
+
+class ContentItemViewHolder(val root: View) : RecyclerView.ViewHolder(root) {
+    val title: TextView = root.findViewById(R.id.text_view)
+    val image: ImageView = root.findViewById(R.id.image_view)
+
+    fun bind(response: Movie?) {
+        title.text = response?.title?:"Default Movie"
+        Glide.with(root.context).load(response?.poster?:"")
+            .placeholder(R.drawable.ic_launcher_foreground).into(image)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/movie/utils/AppModule.java
@@ -0,0 +1,27 @@
+package com.example.movie.utils;
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class AppModule {
+    private Context context;
+    public AppModule(Context context) {
+        this.context = context;
+    }
+    @Provides
+    @Singleton
+    Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    NetworkService provideNetworkService() {
+        return new NetworkService();
+    }
+}
